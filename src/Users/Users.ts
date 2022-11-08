59202bbf99e87eca8704a39b2118205fe5cac6b2
Joel Earps <joel.earps@zenobe.com>
2022-11-08 15:36:05 +0000

    feat!: faked user data for User Class

--- src/Users/Users.ts
+++ src/Users/Users.ts
@@ -3,12 +3,22 @@
 
 import faker from 'faker';
 
-class User {
+export class User {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
 
-  constructor() {}
+  constructor() {
+    // When class is created randomly generate name and location
+    this.name = faker.name.firstName();
+    // Lat and long are strings - so need converting to numbers
+    // To direct assignment like below will fail as lat isn't assigned to an object and the properties inside of it
+    this.location;
+    {
+      lat: parseFloat(faker.address.latitude());
+      lng: parseFloat(faker.address.longitude());
+    }
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 // Parcel will parse html - will load the ts file in the script file and then parse the files
 // Creates all the dependencies etc - check the console to see the new src tag loaded
-console.log("Hello There");
+// Anytime we export using export keyword, it will be exported as an object so we can do a multi export and input whilst being selective about it
+// Different to export default - it will be the default export and therefore can be named anything
+// In TS default is not really used
+import { User } from './Users/Users';
