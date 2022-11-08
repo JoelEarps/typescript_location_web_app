be6a78f0962e1b8804b4d609e7a05e387ad6f9aa
Joel Earps <joel.earps@zenobe.com>
2022-11-08 15:00:29 +0000

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+// Parcel will parse html - will load the ts file in the script file and then parse the files
+// Creates all the dependencies etc - check the console to see the new src tag loaded
+console.log("Hello There");
