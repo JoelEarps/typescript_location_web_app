bda258591526a51eefdc2f304af18b81ce10e64a
Joel Earps <joel.earps@zenobe.com>
2022-11-08 16:58:51 +0000

    feat!: Initial Map Creation
    Please Note the as HTML Element

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/// <reference types="@types/google.maps" />
 // Parcel will parse html - will load the ts file in the script file and then parse the files
 // Creates all the dependencies etc - check the console to see the new src tag loaded
 // Anytime we export using export keyword, it will be exported as an object so we can do a multi export and input whilst being selective about it
@@ -6,8 +7,9 @@
 import { User } from './Users/Users';
 import { Company } from './Company/Company';
 
-const user = new User();
-const company = new Company();
-
-console.log(user);
-console.log(company);
+// Maps is a class - requires mapDiv - reference to a html element where we present and house the map
+let map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  // This is required - even thought it says optional
+  center: { lat: 52.5, lng: 0 },
+  zoom: 8,
+});
